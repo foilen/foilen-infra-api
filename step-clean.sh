4d13d405d037bcb25fb8f450460e543cd3ddb14a
Simon Levesque <simon@foilen.com>
2018-07-02 15:44:33 -0400

    Let unix user id be a long

--- step-clean.sh
+++ step-clean.sh
@@ -5,5 +5,5 @@ set -e
 RUN_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $RUN_PATH
 
-echo '----[ Compile (no tests) ]----'
-./gradlew build assemble install -x test
+echo ----[ Clean and Compile ]----
+./gradlew clean
